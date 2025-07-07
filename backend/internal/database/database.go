40f1655c37e13eeb6064ceed8799f539fd81b601
othersidedrl <darelpandeyis@gmail.com>
2025-07-08 00:04:24 +0700

    feat(database): add database connection

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,1 +1,52 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectDB() *sql.DB {
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5432"
+	}
+	username := os.Getenv("POSTGRES_USER")
+	if username == "" {
+		username = "postgres"
+	}
+	password := os.Getenv("POSTGRES_PASSWORD")
+	if password == "" {
+		password = "yourpassword"
+	}
+	psqlDB := os.Getenv("POSTGRES_DB")
+	if psqlDB == "" {
+		psqlDB = "yourdbname"
+	}
+
+	psqlInfo := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, username, password, psqlDB,
+	)
+
+	log.Printf("Connecting to database at %s", psqlInfo)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatalf("Error pinging the database: %v", err)
+	}
+
+	log.Println("✅ Successfully connected to the database!")
+	return db
+}
