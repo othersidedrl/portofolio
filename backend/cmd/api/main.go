30c2589ca64c8f86a88cf1448557dae81e83c966
othersidedrl <darelpandeyis@gmail.com>
2025-07-08 13:03:30 +0700

    feat(main): initialize authentication dependencies and update server router to include auth handler

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -5,17 +5,25 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/othersidedrl/portfolio/backend/internal/auth"
 	"github.com/othersidedrl/portfolio/backend/internal/database"
 	"github.com/othersidedrl/portfolio/backend/internal/server"
+	"github.com/othersidedrl/portfolio/backend/internal/utils"
 )
 
 func main() {
 	_ = godotenv.Load()
+
+	// Initialize dependencies
+	jwt := utils.NewJWTService()
+	authService := auth.NewService(jwt)
+	authHandler := auth.NewHandler(authService)
+
 	PORT := os.Getenv("PORT")
 	database.ConnectDB()
 
-	handler := server.NewRouter()
-	srv := server.StartServer(":"+PORT, handler)
+	router := server.NewRouter(authHandler)
+	srv := server.StartServer(":"+PORT, router)
 
 	log.Printf("🚀 Server running on http://localhost:%s", PORT)
 	log.Fatal(srv.ListenAndServe())
