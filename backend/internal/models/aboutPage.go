bed5db9e9be74a6c25dd19131bdcd0194712802d
othersidedrl <darelpandeyis@gmail.com>
2025-07-08 23:44:14 +0700

    feat(about): add Available field to AboutPage model for availability status

--- backend/internal/models/aboutPage.go
+++ backend/internal/models/aboutPage.go
@@ -13,6 +13,7 @@ type AboutPage struct {
 	Cards        []AboutCard `json:"cards" gorm:"foreignKey:AboutPageID"`
 	GithubLink   string      `json:"github_link"`
 	LinkedinLink string      `json:"linkedin_link"`
+	Available    bool        `json:"available"`
 	UpdatedAt    time.Time   `json:"updated_at"`
 	CreatedAt    time.Time   `json:"created_at"`
 }
